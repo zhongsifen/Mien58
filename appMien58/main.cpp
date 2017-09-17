7ac876b4c4c5ab5df5400c389f63f46f81ec5905
ZHONG SIFEN <zhongsifen@gmail.com>
2017-09-17 18:31:38 +0800

    Mien

---
+++ appMien58/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  appMien58
+//
+//  Created by SIFEN ZHONG on 16/9/2017.
+//  Copyright © 2017 ___ZHONGSIFEN___. All rights reserved.
+//
+
+#include <iostream>
+
+int main_mien(int argc, const char* argv[]);
+int main(int argc, const char * argv[]) {
+	int ret = main_mien(argc, argv);
+	std::cout << "Hello, World!\n";
+	return ret;
+}
