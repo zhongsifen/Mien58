cadab030325e4365c079db2704403b50763845d3
lakeegret <zhongsifen@tom.com>
2017-09-29 14:41:56 +0800

    webcam

--- appMien58/main.cpp
+++ appMien58/main.cpp
@@ -8,11 +8,12 @@
 
 #include <iostream>
 
+int main_webcam();
 int main_detect(int _argc, char** _argv);
 int main_landmark(int _argc, char** _argv);
 int main_recogn(int _argc, char** _argv);
 int main(int argc, char** argv) {
-	int ret = main_landmark(argc, argv);
+	int ret = main_webcam();
 	std::cout << "Hello, World!\n";
 	return ret;
 }
