206d38b63656b5f414853d8be0db82a4df824b5f
ZHONG SIFEN <zhongsifen@gmail.com>
2017-10-03 14:34:37 +0800

    detect

--- Mien58/Mien58.cpp
+++ Mien58/Mien58.cpp
@@ -11,19 +11,16 @@
 #include <dlib/opencv.h>
 #include <opencv2/imgproc.hpp>
 
-void showFace(cv::Mat& img, Face& face) {
-	cv::rectangle(img, face.box, cv::Scalar(0x00, 0x00, 0xF0));
+void showPoints(cv::Mat& img, std::vector<cv::Point>& points) {
+	for (auto p=points.begin(); p<points.end(); ++p) {
+		cv::circle(img, *p, 2, cv::Scalar(0xF0, 0x00, 0x00));
+	}
 }
 
-//bool
-//Mien58::detect(cv::Mat &img, std::vector<cv::Rect> &faces) {
-//	cv_image<bgr_pixel> cimg(img);
-//	std::vector<std::pair<double, rectangle> > final_dets;
-//	int n = (int)dets.size();		if (n < 1) return false;
-//	fdlib(dets, faces);
-//	
-//	return true;
-//}
+void showFace(cv::Mat& img, Face& face) {
+	cv::rectangle(img, face.box, cv::Scalar(0x00, 0x00, 0xF0));
+	showPoints(img, face.parts);
+}
 
 Mien58::Mien58() {
 	_fd = dlib::get_frontal_face_detector();
@@ -32,25 +29,22 @@ Mien58::Mien58() {
 
 bool
 Mien58::detect(cv::Mat & cvmat, std::vector<Face> &faces) {
-	std::vector<std::pair<double, dlib::rectangle>> dets;
-//	std::vector<dlib::rectangle> dets;
+	std::vector<std::pair<double, dlib::full_object_detection>> dets;
 	dlib::cv_image<dlib::bgr_pixel> dlimg(cvmat);
-//	dets = _fd(dlimg);
 	_fd(dlimg, dets);
 	
 	int n = (int)dets.size();		if (n < 1) return false;
 	faces.resize(n);
 	for (int i=0; i<n; i++) {
-		wdlib::fdlib(dets[i].second, faces[i].box);
-//		miens[i].weight = dets[i].first;
-//		dlib::full_object_detection* fo = &dets[i].second;
-//		fdlib(fo->get_rect(), miens[i].box);
-//		int p = (int)fo->num_parts();
-//		std::vector<cv::Point>* lp = &miens[i].parts;
-//		lp->resize(p);
-//		for (int k=0; k<p; ++k) {
-//			fdlib(fo->part(k), (*lp)[k]);
-//		}
+		faces[i].weight = dets[i].first;
+		dlib::full_object_detection* fo = &dets[i].second;
+		wdlib::fdlib(fo->get_rect(), faces[i].box);
+		int p = (int)fo->num_parts();
+		std::vector<cv::Point>* lp = &faces[i].parts;
+		lp->resize(p);
+		for (int k=0; k<p; ++k) {
+			wdlib::fdlib(fo->part(k), (*lp)[k]);
+		}
 	}
 	
 	return true;
